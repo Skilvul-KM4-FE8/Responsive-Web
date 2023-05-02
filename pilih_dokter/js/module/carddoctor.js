ad31fe2c815d57b59000f8a8182d8a6383987905
rafai123 <rafaimhd123@gmail.com>
2023-05-02 22:28:31 +0700

    add header, card list in html, and add logic to insert list card of doctors from api to html

---
+++ pilih_dokter/js/module/carddoctor.js
@@ -0,0 +1,17 @@
+export function cardDoctor(doctor) {
+  return    `<div class="container mt-3">
+                      <div class="row p-3 border rounded shadow-sm stroke-card-border card-background">
+                          <div class="col-md-2 d-flex justify-content-center align-items-center d-block">
+                              <img src="${doctor.avatar}" alt="image of ${doctor.name}" class="rounded-circle img-fluid mx-auto">
+                          </div>
+                          <div class="col-md-8">
+                              <h4 class="text-carevul ms-0 mt-2"><b> dr. ${doctor.name}</b></h4>
+                              <p class="text-secondary">${doctor.specialist} <br> ${doctor.hospital}</p>
+                              <h5><span class="text-carevul">${doctor.totalPatient} Pasien</span> telah buat janji dengan dokter ini</h5>
+                          </div>
+                          <div class="col-md d-flex justify-content-center align-items-center d-block">
+                              <a href="${doctor.id}" class="btn color-carevul-gradient text-white mx-auto px-5 py-2">Buat Janji</a>
+                          </div>
+                      </div>
+                  </div>`;
+}
