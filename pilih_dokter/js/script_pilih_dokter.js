7d8e9ba71c5005ef70dbc12cbaa707ec9f1a7346
rafai123 <rafaimhd123@gmail.com>
2023-05-02 11:04:52 +0700

    refactor logic for get data doctors from mock API

--- pilih_dokter/js/script_pilih_dokter.js
+++ pilih_dokter/js/script_pilih_dokter.js
@@ -2,8 +2,7 @@
 import { getUrlVars } from "./module/geturl.js";
 
 // get data from mock API
-const doctors = fetch(`https://64506b72a3221969114a2d25.mockapi.io/doctors?category=${getUrlVars().category}`);
-doctors
+const doctors = fetch(`https://64506b72a3221969114a2d25.mockapi.io/doctors?category=${getUrlVars().category}`)
   .then((response) => response.json())
   .then((result) => console.log(result))
   .catch((e) => console.log(e.response.Text));
