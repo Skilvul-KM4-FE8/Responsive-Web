0c642f7d32b6649b489ce9cdf72a0977745985f4
Syifaaahdr29 <syifaaahdr29@gmail.com>
2023-05-03 00:09:10 +0700

    modify data and add image on card

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ fetch("https://6450d733e1f6f1bb22a05272.mockapi.io/medical-article")
     result.forEach((post, index) => {
         const card = `
         <div class="card" id="card">
-            <img class="card-img-top" alt="...">
+            <img src="${post.img}" class="card-img-top">
             <div class="card-body">
                 <h5 class="card-title">${post.title}</h5>
                 <p class="card-text">${post.desc}</p>
