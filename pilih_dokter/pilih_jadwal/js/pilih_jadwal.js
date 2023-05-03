b103ae2b775f66cf2453f8ac245526d3f3d36dd3
rafai123 <rafaimhd123@gmail.com>
2023-05-03 17:40:41 +0700

    add a dynamic calendar

---
+++ pilih_dokter/pilih_jadwal/js/pilih_jadwal.js
@@ -0,0 +1,14 @@
+// import module
+import { getUrlVars } from "../../js/module/geturl.js";
+import { calendar } from "./module/calendar.js";
+
+// get data from mock API
+fetch(`https://64506b72a3221969114a2d25.mockapi.io/doctors?id=${getUrlVars().id}&page=1&limit=1`)
+  .then((response) => response.json())
+  .then((result) => {
+    console.log(result[0])
+  })
+  .catch((e) => console.log(e.response.Text));
+
+
+calendar()
