313d736ecdc0e1ee3c12b06b69ebea0c5808e872
rafai123 <rafaimhd123@gmail.com>
2023-05-05 00:07:02 +0700

    fetch api to get doctor's data for check schedule

--- pilih_dokter/pilih_jadwal/check_jadwal/js/check_jadwal.js
+++ pilih_dokter/pilih_jadwal/check_jadwal/js/check_jadwal.js
@@ -1,9 +1,16 @@
-
 const STORAGE_KEY = "booked-doctor";
 
+const formBooked = document.querySelector("#form-book");
+
 const tempData = JSON.parse(localStorage.getItem("temp-booked-data"));
+console.log(tempData.name);
 
+// fetch the API for getting the doctor's data
+fetch(`https://64506b72a3221969114a2d25.mockapi.io/doctors?id=${tempData.id}&page=1&limit=1`)
+  .then((response) => response.json())
+  .then((d) => {
+    let data = d[0]
+    console.log(data)
 
-
-
-
+    
+  });
