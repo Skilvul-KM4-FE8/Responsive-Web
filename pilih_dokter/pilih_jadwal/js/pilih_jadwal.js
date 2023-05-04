7a433e2a1992c775d7caf617c6cd27b53a214165
rafai123 <rafaimhd123@gmail.com>
2023-05-04 23:28:03 +0700

    false

--- pilih_dokter/pilih_jadwal/js/pilih_jadwal.js
+++ pilih_dokter/pilih_jadwal/js/pilih_jadwal.js
@@ -4,6 +4,7 @@ import { calendar } from "./module/calendar.js";
 import { navbarHTML } from "./module/navbar_html.js";
 import { cardAboutDoctor } from "./module/card_about_doctor.js";
 import { eventBtnBooked } from "./module/event_button_booked.js";
+import { containerScheduleHTML } from "./module/container_schedule_html.js";
 
 const STORAGE_KEY = "temp-booked-data";
 
@@ -77,23 +78,7 @@ fetch(`https://64506b72a3221969114a2d25.mockapi.io/doctors?id=${getUrlVars().id}
 
       let stringifiedValue = JSON.stringify(value);
 
-      containerSchedule.innerHTML += `<div class="form-check ms-0 py-3 px-5 border rounded mt-1 shadow-sm  border-2">
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-1 d-flex justify-content-center align-items-center">
-                                            <input class="form-check-input" type="radio" name="book-date" id="book-date-${date}" style="font-size: 25px; border: 2px solid #3655D5;" value='${stringifiedValue}' ${
-        date == thisDate ? "required" : ""
-      }>
-                                        </div>
-                                        <div class="col">
-                                            <label class="form-check-label" for="book-date-${date}">
-                                                <p class="lead mt-3">${date}, ${now[0]} ${now[1]} ${now[3]} </p>
-                                                <h5 class="text-carevul">${nextHour}:00 - ${nextHour + 1}:00</h5>
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                              </div>`;
+      containerSchedule.innerHTML += containerScheduleHTML(date,stringifiedValue,tomorrow,now,)
 
       const radioBtnValue = Array.from(document.querySelectorAll("input[name = book-date]"));
       radioBtnValue.forEach((btn) => {
