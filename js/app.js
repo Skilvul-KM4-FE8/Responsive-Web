15451374a4eb8c7f26521474a3322df531e7a30d
alfito <alfitosantosa@gmail.com>
2023-05-05 23:28:33 +0700

    ngerapihin file landing page

--- js/app.js
+++
@@ -1,8 +0,0 @@
-let navbar = document.getElementById("navbar");
-window.onscroll = function () {
-  if (document.documentElement.scrollTop > 20) {
-    navbar.classList.add("scroll-on");
-  } else {
-    navbar.classList.remove("scroll-on");
-  }
-};
