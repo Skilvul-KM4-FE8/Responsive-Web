29714314e4a4320a6ea156b9deb0b07084294486
rafai123 <rafaimhd123@gmail.com>
2023-05-06 02:19:14 +0700

    add all links in footer and navbar

--- pilih_dokter/js/script_pilih_dokter.js
+++ pilih_dokter/js/script_pilih_dokter.js
@@ -15,3 +15,13 @@ fetch(`https://64506b72a3221969114a2d25.mockapi.io/doctors?category=${getUrlVars
   .catch((e) => console.log(e.response.Text));
 
 document.getElementById('banner').innerHTML = navbarHTML()
+
+// Ubah status Login di navbar
+const sessionName = sessionStorage.getItem("lastname");
+const sessionEmail = sessionStorage.getItem("email");
+const sessionId = sessionStorage.getItem("id");
+const navbarLoginOrNot = document.querySelector("#navbar-login-or-not")
+
+if (sessionName && sessionEmail && sessionId) {
+  navbarLoginOrNot.innerHTML = `<h5>${sessionName}</h5>`
+} 
